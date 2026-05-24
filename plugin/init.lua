8e91ada90c52a560e5cbc975cc8832aeadf86185
Marc Weber <marco-oweber@gmx.de>
2026-05-24 18:47:40 +0200

    u

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,2 +1,3 @@
 -- vim.api.nvim_create_user_command("JuliaToQFFromTerminal", function() require'lua-terminal-to-qf':setup_julia_walkback() end, {})
-vim.api.nvim_create_user_command("ZSHWithJuliaErrorWatching", function() require'watched-terminal-process-finding-isabelle-errors':launch_watched_zsh() end, {})
+vim.api.nvim_create_user_command("ZSHWithJuliaErrorWatchingOld", function() require'watched-terminal-process-finding-julia-errors':launch_watched_zsh() end, {})
+vim.api.nvim_create_user_command("ZSHWithJuliaErrorWatching", function() require'watched-terminal-process-finding-julia-errors2':launch_julia_watcher() end, {})
